6869fe05500eca7b7806fb88d524f2b3f99379c2
Raul Sierra <j.raulsierra@gmail.com>
2024-05-21 11:40:37 -0600

    feat(section-6): catching errors when it is a format error in out binding formating. using BindingResult

--- src/main/java/org/rsierra/controllers/VacancyController.java
+++ src/main/java/org/rsierra/controllers/VacancyController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,7 +35,13 @@ public class VacancyController {
     }
 
     @PostMapping("/saveVacancy")
-    public String saveVacancy(Vacancy vacancy) {
+    public String saveVacancy(Vacancy vacancy, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            for (ObjectError objectError : bindingResult.getAllErrors()) {
+                System.out.println(objectError.getDefaultMessage());
+            }
+            return "vacancy/formVacancy";
+        }
         vacancyService.saveVacancy(vacancy);
         return "vacancy/listVacancies";
     }
