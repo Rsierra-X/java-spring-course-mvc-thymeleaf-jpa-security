c8bbc44b0c72abdb66a1787bd4793eeaaa441738
Raul Sierra <j.raulsierra@gmail.com>
2024-05-31 14:10:21 -0600

    feat(section-13): SecurityFilterChain

--- src/main/java/org/rsierra/security/DatabaseWebSecurity.java
+++ src/main/java/org/rsierra/security/DatabaseWebSecurity.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -38,7 +39,7 @@ public class DatabaseWebSecurity {
                 .anyRequest().authenticated());
 
         // El formulario de Login no requiere autenticacion
-        http.formLogin(form -> form.permitAll());
+        http.formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
 
         return http.build();
     }
