e709a6a5cd7794859a188a9f88a8e1a837c7a0ce
Raul Sierra <j.raulsierra@gmail.com>
2024-05-31 12:46:38 -0600

    feat(section-13): SecurityFilterChain

--- src/main/java/org/rsierra/security/DatabaseWebSecurity.java
+++ src/main/java/org/rsierra/security/DatabaseWebSecurity.java
@@ -2,9 +2,11 @@ package org.rsierra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -20,4 +22,25 @@ public class DatabaseWebSecurity {
                 "inner join profile p on p.id = up.idProfile  " + "where u.username = ?");
         return users;
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize -> authorize
+
+                // Los recursos estáticos no requieren autenticación
+                .requestMatchers("/bootstrap/**", "/images/**", "/tinymce/**", "/logos/**").permitAll()
+
+                // Las vistas públicas no requieren autenticación
+                .requestMatchers("/", "/signup", "/search", "/vacantes/view/**").permitAll()
+
+                // Todas las demás URLs de la Aplicación requieren autenticación
+                .anyRequest().authenticated());
+
+        // El formulario de Login no requiere autenticacion
+        http.formLogin(form -> form.permitAll());
+
+        return http.build();
+    }
+
 }
