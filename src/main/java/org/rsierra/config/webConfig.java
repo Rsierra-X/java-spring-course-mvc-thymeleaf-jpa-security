85362e53101901a68fc75a256cbb7714fc1cce31
Raul Sierra <j.raulsierra@gmail.com>
2024-06-03 11:38:53 -0600

    feat(section-14): setting webConfig for resourcesHandler

---
+++ src/main/java/org/rsierra/config/webConfig.java
@@ -0,0 +1,18 @@
+package org.rsierra.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webConfig implements WebMvcConfigurer {
+    @Value("${companiesApp.path.images}")
+    private String rutaImagenes;
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //registry.addResourceHandler("/logos/**").addResourceLocations("file:/empleos/img-vacantes/"); // Linux
+        registry.addResourceHandler("/logos/**").addResourceLocations("file:C:/empleos/img-vacancies/"); // Windows
+        registry.addResourceHandler("/logos/**").addResourceLocations("file:" + rutaImagenes); // Windows
+    }
+}
