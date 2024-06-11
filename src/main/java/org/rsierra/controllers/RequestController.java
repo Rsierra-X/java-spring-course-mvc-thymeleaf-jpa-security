6e0248da38a23453e5ef4b6c4e5d848290d7e2a2
Raul Sierra <j.raulsierra@gmail.com>
2024-06-11 14:11:16 -0600

    feat(section-15): set request controller

---
+++ src/main/java/org/rsierra/controllers/RequestController.java
@@ -0,0 +1,37 @@
+package org.rsierra.controllers;
+
+import org.rsierra.models.Request;
+import org.rsierra.models.Vacancy;
+import org.rsierra.repository.RequestRepository;
+import org.rsierra.service.IRequestService;
+import org.rsierra.service.IVacancyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/request")
+public class RequestController {
+
+    @Autowired
+    private IRequestService requestService;
+
+    @Autowired
+    private IVacancyService vacancyService;
+
+    @GetMapping("/create/{id}")
+    public String create(Request request,@PathVariable int id, Model model) {
+        Vacancy vacancy = vacancyService.getVacancyById(id);
+        model.addAttribute("vacancy", vacancy);
+        return "request/formRequest";
+    }
+
+    @PostMapping("/save")
+    public String save(Request request, Model model) {
+        return "redirect:/";
+    }
+}
