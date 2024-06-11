b3a6e35d6b05a32115c7795c8d7ae0f64158a26b
Raul Sierra <j.raulsierra@gmail.com>
2024-06-11 12:18:29 -0600

    feat(section-15): create the request service and jpa repository

---
+++ src/main/java/org/rsierra/service/db/RequestServiceJpa.java
@@ -0,0 +1,45 @@
+package org.rsierra.service.db;
+
+import org.rsierra.models.Request;
+import org.rsierra.repository.RequestRepository;
+import org.rsierra.service.IRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RequestServiceJpa implements IRequestService {
+
+    @Autowired
+    private RequestRepository requestRepository;
+
+    @Override
+    public void saveRequest(Request request) {
+        requestRepository.save(request);
+    }
+
+    @Override
+    public void deleteRequest(Request request) {
+        requestRepository.delete(request);
+    }
+
+    @Override
+    public List<Request> getAllRequests() {
+        return requestRepository.findAll();
+    }
+
+    @Override
+    public Request getRequestById(int id) {
+        Optional<Request> request = requestRepository.findById(id);
+        return request.orElse(null);
+    }
+
+    @Override
+    public Page<Request> getAllRequests(Pageable pageable) {
+        return requestRepository.findAll(pageable);
+    }
+}
